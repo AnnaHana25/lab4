c3d4d1930898e1aef6a8c160ae9df09e8e727028
AnnaHana25 <hunguga2005@gmail.com>
2023-11-28 21:28:51 +0200

    РЎС‚РІРѕСЂРёР»Р° Р±Р»РѕРєРё

---
+++ src/Content.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Content = ({ title }) => {
+  return (
+    <div style={{ border: "1px solid black" }}>
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = ({ title }) => {
+  return (
+    <div style={{ border: "1px solid black" }}>
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+export default Footer;
